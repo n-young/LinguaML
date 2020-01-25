00c8bbd5c5cdf4e556195a7c47a0258e13bb375e
Jed Fox <git@twopointzero.us>
2020-01-25 16:46:54 -0500

    Upgrade navigator, card stuff

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
+import { NavigationNativeContainer } from '@react-navigation/native';
+import { enableScreens } from 'react-native-screens';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import AppNavigator from './navigation/AppNavigator';
+import HomeScreen from './screens/HomeScreen';
+import LinksScreen from './screens/LinksScreen';
+import CardScreen from './screens/CardScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+enableScreens();
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+HomeScreen.navigationOptions = {
+  tabBarLabel: 'Home',
+};
+
+HomeScreen.path = '';
+
+const LinksStack = createNativeStackNavigator();
+function LinksStackScreen() {
+  return (
+    <LinksStack.Navigator>
+      <LinksStack.Screen
+        name="Cards"
+        component={LinksScreen}
+        options={{ headerLargeTitle: true }}
+      />
+      <LinksStack.Screen name="Card" component={CardScreen} />
+    </LinksStack.Navigator>
+  );
+}
+
+const SettingsStack = createNativeStackNavigator();
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App(props) {
   return (
     <View style={styles.container}>
-      <AppNavigator />
+      <NavigationNativeContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Cards" component={LinksStackScreen} />
+          <Tab.Screen name="Settings" component={SettingsStackScreen} />
+        </Tab.Navigator>
+      </NavigationNativeContainer>
     </View>
   );
 }
